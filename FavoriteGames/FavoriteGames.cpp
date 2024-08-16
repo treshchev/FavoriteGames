5bd958a26af742fa5ff21ec74e420749b7077233
Vladislav Treshchev <letsgovlad@gmail.com>
2024-08-16 17:51:01 +0300

    Created Game

--- FavoriteGames/FavoriteGames.cpp
+++ FavoriteGames/FavoriteGames.cpp
@@ -1,20 +1,72 @@
-// FavoriteGames.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <string>
+#include <vector>
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<std::string>WritedGames;
+	std::string Input;
+
+	while (true)
+	{
+       std::cout << "Write your Favorite Games\n\n" << std::endl;
+
+	   std::cout << "Choose action: "
+		   << "1. Add Game\n"
+		   << "2. Show list of your games\n"
+		   << "3. Deleted games\n"
+		   << "4. Get out\n";
+	   std::cin >> Input;
+
+	   if (Input == "1")
+	   {
+		   std::cout << "Write your game name: ";
+		   std::cin >> Input;
+
+		   WritedGames.push_back(Input);
+		   std::cout << "Your game successfully added!\n";
+	   }
+	   else if (Input == "2")
+	   {
+		   if (WritedGames.empty())
+		   {
+			   std::cout << "Your list is empty";
+		   }
+		   else
+		   {
+			   std::cout << "Your list:";
+			   for (size_t i = 0; i < WritedGames.size(); i++)
+			   {
+				   std::cout << "- " << WritedGames[i] << std::endl;
+			   }
+		   }
+	   }
+	   else if (Input == "3")
+	   {
+		   if (WritedGames.empty())
+		   {
+			   std::cout << "Your list is empty";
+		   }
+		   else
+		   {
+			   std::cout << "Your list:";
+			   for (size_t i = 0; i < WritedGames.size(); i++)
+			   {
+				   std::cout << "- " << WritedGames[i] << std::endl;
+			   }
+		   }
+		   int IndexForDelete;
+		   std::cout << "Choose you game to delete: ";
+		   std::cin >> IndexForDelete;
+
+		   if (IndexForDelete >= 1 && IndexForDelete <= WritedGames.size())
+		   {
+			   WritedGames.erase(WritedGames.begin() + IndexForDelete - 1);
+			   std::cout << "Game has been deleted!\n";
+		   }
+		   else
+		   {
+			   std::cout << "You wrote incorrect number\n" << std::endl;
+		   }
+	   }
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
